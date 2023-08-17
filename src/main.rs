efc26ed573a67ee0d66f85539eec3d173eb08f92
Kylar <kylar.osborne@gmail.com>
2023-08-17 15:43:12 +1000

    Multiple immutable references, mutable reference in function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
+    
     io::stdin().read_line(&mut input);
+    let s1 = &input;
+    let s2 = &input;
+    println!("{} {}", s1, s2);
+    some_fn(&mut input);
 
     let mut mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on mars: {}kg : {}", calculate_weight_on_mars(mars_weight), input);
@@ -12,3 +17,8 @@ fn main() {
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
+
+fn some_fn(s: &mut String){
+    s.push_str("Goodbye");
+}
+ 
