6b8a89565af38a21ab8b70321ec0ca6f62abce62
Kylar <kylar.osborne@gmail.com>
2023-08-17 16:08:27 +1000

    Cleaned and unwrapped result

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@ use std::io;
 fn main() {
     let mut input = String::new();
     
-    io::stdin().read_line(&mut input);
-    let s1 = &input;
-    let s2 = &input;
-    println!("{} {}", s1, s2);
-    some_fn(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
 
-    let mut mars_weight = calculate_weight_on_mars(100.0);
+    let mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on mars: {}kg : {}", calculate_weight_on_mars(mars_weight), input);
 
 }
@@ -17,8 +13,3 @@ fn main() {
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
-
-fn some_fn(s: &mut String){
-    s.push_str("Goodbye");
-}
- 
