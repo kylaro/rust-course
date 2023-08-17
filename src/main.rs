2a5e1d8f498a8af51ae916d5dd80d6adf5748428
Kylar <kylar.osborne@gmail.com>
2023-08-17 14:40:05 +1000

    Created mars function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let num = 25;
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 1000.0;
+
+    println!("Weight on mars: {}kg", calculate_weight_on_mars(100.0));
+
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
