c62ecff575fde24dbf1147168d2cfc0e7c3602e2
Kylar <kylar.osborne@gmail.com>
2023-08-17 14:53:54 +1000

    Add user input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-fn main() {
-    let num = 25;
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
+use std::io;
 
-    println!("Weight on mars: {}kg", calculate_weight_on_mars(100.0));
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on mars: {}kg : {}", calculate_weight_on_mars(mars_weight), input);
 
 }
 
